d425314c78abf5ecd083e5722a6b01c54d529912
1stofthekumans <36697738+1stofthekumans@users.noreply.github.com>
2026-03-05 22:34:58 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:29:46.943Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T19:34:57.914Z
+Commit: Patch API endpoint error handling
