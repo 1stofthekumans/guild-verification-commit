97108947584d151855b06417c8441f8626d86c86
1stofthekumans <36697738+1stofthekumans@users.noreply.github.com>
2026-03-05 22:37:02 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:36:57.199Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T19:37:01.454Z
+Commit: Fix broken links in documentation
