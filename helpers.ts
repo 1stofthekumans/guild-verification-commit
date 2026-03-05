a31cdc295d9ece93bb21d0cd6dc7dbd05d1b11ff
1stofthekumans <36697738+1stofthekumans@users.noreply.github.com>
2026-03-05 22:36:45 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T19:36:26.678Z
-Commit: Fix race condition in async operations
+Timestamp: 2026-03-05T19:36:44.880Z
+Commit: Patch API endpoint error handling
